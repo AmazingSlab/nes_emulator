a02c0a5969521e011c92595d704e8703f13bb21f
AmazingSlab <thomasflaknelien@gmail.com>
2023-12-09 15:23:32 +0100

    limit framerate

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use nes_emulator::{Bus, Cartridge, Cpu, Ppu};
 use sdl2::{event::Event, keyboard::Keycode, pixels::PixelFormatEnum};
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, rc::Rc, time::Duration};
 
 const SCALE: u32 = 4;
+const FPS: u64 = 60;
 
 pub fn main() {
     let mut args = std::env::args();
@@ -10,6 +11,7 @@ pub fn main() {
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
+    let timer_subsystem = sdl_context.timer().unwrap();
 
     let window = video_subsystem
         .window("NES Emulator", 256 * SCALE, 240 * SCALE)
@@ -17,7 +19,7 @@ pub fn main() {
         .build()
         .unwrap();
 
-    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    let mut canvas = window.into_canvas().build().unwrap();
     canvas.set_scale(SCALE as f32, SCALE as f32).unwrap();
     let texture_creator = canvas.texture_creator();
     let mut texture = texture_creator
@@ -95,10 +97,19 @@ pub fn main() {
         }
 
         if run_emulation {
+            let desired_delta = 1000 / FPS;
+            let frame_start = timer_subsystem.ticks64();
+
             while !ppu.borrow().is_frame_ready {
                 Bus::clock(cpu.clone(), ppu.clone());
             }
             ppu.borrow_mut().is_frame_ready = false;
+
+            let frame_end = timer_subsystem.ticks64();
+            let delta = frame_end - frame_start;
+            if delta < desired_delta {
+                std::thread::sleep(Duration::from_millis(desired_delta - delta));
+            }
         }
 
         texture
