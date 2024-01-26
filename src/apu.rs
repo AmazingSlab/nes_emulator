6239436687f672c218a6c28f073c290535fe2221
AmazingSlab <thomasflaknelien@gmail.com>
2024-01-26 10:06:13 +0100

    deduplicate pulse channels

--- src/apu.rs
+++ src/apu.rs
@@ -4,17 +4,8 @@ const BUFFER_SIZE: usize = 8 * 1024;
 pub struct Apu {
     audio_buffer: Vec<i16>,
 
-    pulse_1_duty_cycle: u8,
-    pulse_1_timer: u16,
-    pulse_1_timer_reload: u16,
-    pulse_1_sequence_counter: u8,
-    pulse_1_output: i16,
-
-    pulse_2_duty_cycle: u8,
-    pulse_2_timer: u16,
-    pulse_2_timer_reload: u16,
-    pulse_2_sequence_counter: u8,
-    pulse_2_output: i16,
+    pulse_1: PulseChannel,
+    pulse_2: PulseChannel,
 
     clock_timer: usize,
 }
@@ -23,60 +14,17 @@ impl Apu {
     pub fn new() -> Self {
         Self {
             audio_buffer: Vec::with_capacity(BUFFER_SIZE),
-            pulse_1_duty_cycle: 0b00000001,
-            pulse_2_duty_cycle: 0b00000001,
             ..Default::default()
         }
     }
 
     pub fn clock(&mut self) {
-        self.pulse_1_timer = self.pulse_1_timer.wrapping_sub(1) & 0x07FF;
-        if self.pulse_1_timer == 0x07FF {
-            let bit_mux = 0x80 >> self.pulse_1_sequence_counter;
-            let sample = if (self.pulse_1_duty_cycle & bit_mux) != 0 {
-                1000
-            } else {
-                -1000
-            };
-            let sample = if self.pulse_1_timer_reload > 8 {
-                sample
-            } else {
-                0
-            };
-            self.pulse_1_output = sample;
-            if self.pulse_1_sequence_counter > 0 {
-                self.pulse_1_sequence_counter -= 1;
-            } else {
-                self.pulse_1_sequence_counter = 7;
-            }
-            self.pulse_1_timer = self.pulse_1_timer_reload + 1;
-        }
-
-        self.pulse_2_timer = self.pulse_2_timer.wrapping_sub(1) & 0x07FF;
-        if self.pulse_2_timer == 0x07FF {
-            let bit_mux = 0x80 >> self.pulse_2_sequence_counter;
-            let sample = if (self.pulse_2_duty_cycle & bit_mux) != 0 {
-                1000
-            } else {
-                -1000
-            };
-            let sample = if self.pulse_2_timer_reload > 8 {
-                sample
-            } else {
-                0
-            };
-            self.pulse_2_output = sample;
-            if self.pulse_2_sequence_counter > 0 {
-                self.pulse_2_sequence_counter -= 1;
-            } else {
-                self.pulse_2_sequence_counter = 7;
-            }
-            self.pulse_2_timer = self.pulse_2_timer_reload + 1;
-        }
+        self.pulse_1.clock();
+        self.pulse_2.clock();
 
         if self.clock_timer % 20 == 0 {
             self.audio_buffer
-                .push(self.pulse_1_output + self.pulse_2_output);
+                .push(self.pulse_1.output + self.pulse_2.output);
         }
         self.clock_timer += 1;
     }
@@ -95,7 +43,7 @@ impl Apu {
     pub fn cpu_write(&mut self, addr: u16, data: u8) {
         match addr {
             0x4000 => {
-                self.pulse_1_duty_cycle = match (data >> 6) & 0x03 {
+                self.pulse_1.duty_cycle = match (data >> 6) & 0x03 {
                     0 => 0b00000001,
                     1 => 0b00000011,
                     2 => 0b00001111,
@@ -103,14 +51,14 @@ impl Apu {
                     _ => unreachable!(),
                 }
             }
-            0x4002 => self.pulse_1_timer_reload = data as u16,
+            0x4002 => self.pulse_1.timer_reload = data as u16,
             0x4003 => {
-                self.pulse_1_timer_reload =
-                    (self.pulse_1_timer_reload & 0x00FF) | ((data as u16 & 0x07) << 8);
-                self.pulse_1_timer = self.pulse_1_timer_reload;
+                self.pulse_1.timer_reload =
+                    (self.pulse_1.timer_reload & 0x00FF) | ((data as u16 & 0x07) << 8);
+                self.pulse_1.timer = self.pulse_1.timer_reload;
             }
             0x4004 => {
-                self.pulse_2_duty_cycle = match (data >> 6) & 0x03 {
+                self.pulse_2.duty_cycle = match (data >> 6) & 0x03 {
                     0 => 0b00000001,
                     1 => 0b00000011,
                     2 => 0b00001111,
@@ -118,13 +66,59 @@ impl Apu {
                     _ => unreachable!(),
                 }
             }
-            0x4006 => self.pulse_2_timer_reload = data as u16,
+            0x4006 => self.pulse_2.timer_reload = data as u16,
             0x4007 => {
-                self.pulse_2_timer_reload =
-                    (self.pulse_2_timer_reload & 0x00FF) | ((data as u16 & 0x07) << 8);
-                self.pulse_2_timer = self.pulse_2_timer_reload;
+                self.pulse_2.timer_reload =
+                    (self.pulse_2.timer_reload & 0x00FF) | ((data as u16 & 0x07) << 8);
+                self.pulse_2.timer = self.pulse_2.timer_reload;
             }
             _ => (),
         }
+    }
+}
+
+struct PulseChannel {
+    duty_cycle: u8,
+    timer: u16,
+    timer_reload: u16,
+    sequence_counter: u8,
+    output: i16,
+}
+
+impl PulseChannel {
+    pub fn new() -> Self {
+        Self {
+            duty_cycle: 0b00000001,
+            timer: 0,
+            timer_reload: 0,
+            sequence_counter: 0,
+            output: 0,
+        }
+    }
+
+    pub fn clock(&mut self) {
+        self.timer = self.timer.wrapping_sub(1) & 0x07FF;
+        if self.timer == 0x07FF {
+            let bit_mux = 0x80 >> self.sequence_counter;
+            let sample = if (self.duty_cycle & bit_mux) != 0 {
+                1000
+            } else {
+                -1000
+            };
+            let sample = if self.timer_reload > 8 { sample } else { 0 };
+            self.output = sample;
+            if self.sequence_counter > 0 {
+                self.sequence_counter -= 1;
+            } else {
+                self.sequence_counter = 7;
+            }
+            self.timer = self.timer_reload + 1;
+        }
+    }
+}
+
+impl Default for PulseChannel {
+    fn default() -> Self {
+        Self::new()
     }
 }
