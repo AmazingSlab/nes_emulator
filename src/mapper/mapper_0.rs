c7d914cb4f1e2fdd53bad1f60eff0da98411aded
AmazingSlab <thomasflaknelien@gmail.com>
2024-02-06 16:03:19 +0100

    implement chr ram for mapper 0

--- src/mapper/mapper_0.rs
+++ src/mapper/mapper_0.rs
@@ -5,6 +5,7 @@ pub struct Mapper0 {
     chr_rom: Vec<u8>,
     variant: NromVariant,
     mirror_flag: u8,
+    has_chr_ram: bool,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -26,11 +27,19 @@ impl Mapper0 {
             blocks => return Err(format!("{blocks} is not a valid block size for mapper 0")),
         };
 
+        let has_chr_ram = chr_rom.is_empty();
+        let chr_rom = if has_chr_ram {
+            vec![0; 8 * 1024]
+        } else {
+            chr_rom.into()
+        };
+
         Ok(Self {
             prg_rom: prg_rom.into(),
-            chr_rom: chr_rom.into(),
+            chr_rom,
             variant,
             mirror_flag,
+            has_chr_ram,
         })
     }
 
@@ -52,15 +61,16 @@ impl Mapper for Mapper0 {
     fn cpu_write(&mut self, _addr: u16, _data: u8) {}
 
     fn ppu_read(&self, addr: u16) -> u8 {
-        if !self.chr_rom.is_empty() {
-            let addr = addr as usize & 0x1FFF;
-            self.chr_rom[addr]
-        } else {
-            0
-        }
+        let addr = addr as usize & 0x1FFF;
+        self.chr_rom[addr]
     }
 
-    fn ppu_write(&mut self, _addr: u16, _data: u8) {}
+    fn ppu_write(&mut self, addr: u16, data: u8) {
+        if self.has_chr_ram {
+            let addr = addr as usize & 0x1FFF;
+            self.chr_rom[addr] = data;
+        }
+    }
 
     fn mirroring(&self) -> Mirroring {
         if self.mirror_flag == 0 {
